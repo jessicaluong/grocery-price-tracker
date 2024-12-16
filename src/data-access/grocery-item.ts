4e6a649129f67f8a31aa44343337174ec1aeea0e
jessicaluong <96930184+jessicaluong@users.noreply.github.com>
2024-12-16 15:21:47 -0800

    Resolve Unit type error

--- src/data-access/grocery-item.ts
+++ src/data-access/grocery-item.ts
@@ -1,8 +1,14 @@
 import prisma from "@/lib/db";
+import { Unit } from "@/lib/types";
 
 export async function getGroceryItems() {
-  const groceryItems = await prisma.groceryItem.findMany();
-  return groceryItems || [];
+  const items = await prisma.groceryItem.findMany();
+  return (
+    items.map((item) => ({
+      ...item,
+      unit: item.unit as Unit,
+    })) || []
+  );
 }
 
 export async function updateGroceryItems() {}
