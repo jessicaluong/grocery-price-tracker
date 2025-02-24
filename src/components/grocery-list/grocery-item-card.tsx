bd469f40739cbe31113129b2e396dfeedfab32e9
jessicaluong <96930184+jessicaluong@users.noreply.github.com>
2025-02-24 13:53:41 -0800

    Add per unit prices to group view

--- src/components/grocery-list/grocery-item-card.tsx
+++ src/components/grocery-list/grocery-item-card.tsx
@@ -5,7 +5,11 @@ import { ItemWithView } from "@/lib/types";
 import GroceryItemDialog from "./grocery-item-dialog";
 import { useState } from "react";
 import { ItemQuantity } from "./item-quantity";
-import { comparePriceFormat, currencyFormat } from "@/lib/utils";
+import {
+  comparePriceFormat,
+  currencyFormat,
+  getConvertedPrice,
+} from "@/lib/utils";
 import { SaleIndicator } from "./sale-indicator";
 
 type GroceryItemCardProps = ItemWithView;
@@ -57,29 +61,48 @@ export default function GroceryItemCard({ item, view }: GroceryItemCardProps) {
               </>
             )}
             {view === "GROUP" && (
-              <p className="font-semibold tracking-tight flex items-center justify-end">
-                {item.minPrice === item.maxPrice
-                  ? currencyFormat(item.minPrice)
-                  : `${currencyFormat(item.minPrice)}-${currencyFormat(
-                      item.maxPrice
-                    )}`}
-              </p>
+              <>
+                <p className="font-semibold tracking-tight flex items-center justify-end">
+                  {item.minPrice !== item.maxPrice &&
+                    `${currencyFormat(item.minPrice)}-`}
+                  {currencyFormat(item.maxPrice)}
+                </p>
+                <p className="text-sm text-muted-foreground tracking-tighter h-7">
+                  {item.minPrice !== item.maxPrice &&
+                    `${currencyFormat(
+                      getConvertedPrice(
+                        item.count,
+                        item.minPrice,
+                        item.amount,
+                        item.unit
+                      )
+                    )}-`}
+                  {comparePriceFormat(
+                    item.count,
+                    item.maxPrice,
+                    item.amount,
+                    item.unit
+                  )}
+                </p>
+              </>
             )}
             <p className="font-light tracking-tighter">
               {view === "LIST" && item.date.toISOString().split("T")[0]}
               {view === "GROUP" &&
-                `${item.numberOfItems} ${
-                  item.numberOfItems === 1 ? "item" : "items"
+                `${item.priceHistory.length} ${
+                  item.priceHistory.length === 1 ? "item" : "items"
                 }`}
             </p>
           </div>
         </CardContent>
       </Card>
-      <GroceryItemDialog
-        open={isDialogOpen}
-        onOpenChange={setIsDialogOpen}
-        itemWithView={itemWithView}
-      />
+      {isDialogOpen && (
+        <GroceryItemDialog
+          open={isDialogOpen}
+          onOpenChange={setIsDialogOpen}
+          itemWithView={itemWithView}
+        />
+      )}
     </>
   );
 }
