5fa29b71c91ec046cc7adab954e646a1ab922dd9
jessicaluong <96930184+jessicaluong@users.noreply.github.com>
2025-03-10 15:51:10 -0700

    ui(search-bar): Add Search icon to search bar

--- src/app/(dashboard)/groceries/components/header/search-bar.tsx
+++ src/app/(dashboard)/groceries/components/header/search-bar.tsx
@@ -3,6 +3,7 @@
 import { Input } from "@/components/ui/input";
 import { useUrlParams } from "@/lib/hooks";
 import { cn } from "@/lib/utils";
+import { Search } from "lucide-react";
 import { useEffect, useState } from "react";
 
 type SearchBarProps = {
@@ -22,15 +23,15 @@ export function SearchBar({ className }: SearchBarProps) {
   }, [inputValue, updateParam]);
 
   return (
-    <Input
-      className={cn(
-        "w-full sm:flex-1 sm:min-w-[400px] sm:max-w-[500px]",
-        className
-      )}
-      type="search"
-      placeholder="Search by item name and brand"
-      value={inputValue}
-      onChange={(e) => setInputValue(e.target.value)}
-    />
+    <div className={cn("flex items-center relative", className)}>
+      <Search className="absolute left-2 h-4 w-4 text-muted-foreground" />
+      <Input
+        className="pl-8 w-full sm:flex-1 sm:min-w-[400px] sm:max-w-[500px]"
+        type="text"
+        placeholder="Search by item name and brand"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+    </div>
   );
 }
