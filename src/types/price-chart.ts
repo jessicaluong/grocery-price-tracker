31df5eefa999ff371aba674051655f4843983487
jessicaluong <96930184+jessicaluong@users.noreply.github.com>
2025-04-11 14:10:35 -0700

    refactor: remove unused code

--- src/types/price-chart.ts
+++ src/types/price-chart.ts
@@ -3,14 +3,6 @@ export type DateRange = {
   end: Date | null;
 };
 
-// export const TIME_FRAMES = [
-//   { value: "1m", label: "1 Month" }, // x-axis is day (time frame is from beginning to end of that month)
-//   { value: "3m", label: "3 Months" }, // x-axis is months
-//   { value: "6m", label: "6 Months" }, // x-axis is months
-//   { value: "1y", label: "Year" }, // x-axis is months (format as letter or number?)
-//   { value: "all", label: "All Time" }, // x-axis is year only
-// ];
-
 export const TimeFrameValue = {
   ONE_MONTH: "1m",
   THREE_MONTHS: "3m",
@@ -23,10 +15,6 @@ export type TimeFrame = (typeof TimeFrameValue)[keyof typeof TimeFrameValue];
 export const TIME_FRAMES = [
   { value: TimeFrameValue.ONE_MONTH, label: "1M" },
   { value: TimeFrameValue.THREE_MONTHS, label: "3M" },
-<<<<<<< HEAD
-=======
-  { value: TimeFrameValue.SIX_MONTHS, label: "6M" },
->>>>>>> 64b496633abeaec5e2b3e85c7072867218ca6237
   { value: TimeFrameValue.ONE_YEAR, label: "Y" },
   { value: TimeFrameValue.ALL_TIME, label: "All" },
 ];
